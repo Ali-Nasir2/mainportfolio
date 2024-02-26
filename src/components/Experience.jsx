de8dafdf3951e6e10a6f26fcae369c17249dbd36
0xraphael <151480981+0xrphl@users.noreply.github.com>
2024-02-25 22:54:58 -0500

    20240225AlphaV0.7 version.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,9 +4,7 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
-
 import "react-vertical-timeline-component/style.min.css";
-
 import { styles } from "../styles";
 import { experiences } from "../constants";
 import { SectionWrapper } from "../hoc";
@@ -14,6 +12,7 @@ import { textVariant } from "../utils/motion";
 
 const ExperienceCard = ({ experience }) => {
   return (
+
     <VerticalTimelineElement
       contentStyle={{
         background: "#0c0c0c",
@@ -71,15 +70,12 @@ const ExperienceCard = ({ experience }) => {
 const Experience = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} text-center`}>
           What I have done so far
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
           Work Experience.
         </h2>
-      </motion.div>
-
       <div className='mt-20 flex flex-col'>
         <VerticalTimeline>
           {experiences.map((experience, index) => (
