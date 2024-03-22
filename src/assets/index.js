c58f5f79b5f32516e836494067c5d683e154f985
0xraphael <151480981+0xrphl@users.noreply.github.com>
2024-03-22 18:26:38 -0500

    BetaV1.01

--- src/assets/index.js
+++ src/assets/index.js
@@ -18,7 +18,7 @@ import reactjs from "./tech/reactjs.png";
 import MySQL from "./tech/MySQL.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import threejs from "./tech/threejs.png";
 import python from "./tech/python.png";
 import PowerBI from "./tech/PowerBI.png";
 import Tableau from "./tech/Tableau.png";
